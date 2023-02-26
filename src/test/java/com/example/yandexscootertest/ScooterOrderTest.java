b8fd828cf4030c1f8d470d86e355d9db358d32f6
Regina Simankina <si.reginasi@yandex.ru>
2023-02-26 22:40:02 +0600

    sprint 4

---
+++ src/test/java/com/example/yandexscootertest/ScooterOrderTest.java
@@ -0,0 +1,148 @@
+package com.example.yandexscootertest;
+
+import com.example.yandexscootertest.page_object.OrderPageObject;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ScooterOrderTest {
+
+    private static final String EXPECTED = "Заказ оформлен";
+
+    private WebDriver driver;
+
+    private final String day;
+    private final String name;
+    private final String phone;
+    private final String color;
+    private final String surname;
+    private final String address;
+    private final String comment;
+    private final String metroStation;
+    private final String rentalPeriod;
+
+    public ScooterOrderTest(String name,
+                            String phone,
+                            String surname,
+                            String address,
+                            String color,
+                            String comment,
+                            String metroStation,
+                            String rentalPeriod,
+                            String day) {
+        this.name = name;
+        this.phone = phone;
+        this.surname = surname;
+        this.address = address;
+        this.color = color;
+        this.comment = comment;
+        this.metroStation = metroStation;
+        this.rentalPeriod = rentalPeriod;
+        this.day = day;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getLocatorsAndText() {
+        return new Object[][]{
+                {"Иван", "79999999999", "Иванов", "г. Москва, дом 1", "чёрный жемчуг", "", "Бульвар Рокоссовского", "сутки", "1"},
+                {"Сидор", "78888888888", "Сидоров", "г. Москва, дом 2", "серая безысходность", "Комментарий1", "Черкизовская", "двое суток", "9"},
+                {"Петр", "77777777777", "Петров", "г. Москва, дом 3", "чёрный жемчуг", "", "Преображенская площадь", "трое суток", "11"},
+                {"Владимир", "76666666666", "Владимиров", "г. Москва, дом 4", "серая безысходность", "Комментарий2", "Сокольники", "четверо суток", "15"},
+                {"Александр", "75555555555", "Александров", "г. Москва, дом 5", "чёрный жемчуг", "", "Красносельская", "пятеро суток", "23"},
+        };
+    }
+
+    @Before
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void testTopButton() {
+        driver = new ChromeDriver();
+        //Ссылка на страницу Яндекс Самокат
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        OrderPageObject orderPageObject = new OrderPageObject(driver);
+
+        //Нажатие кнопки "Заказать" вверху страницы
+        orderPageObject.clickTopOrderButton();
+
+        test(orderPageObject);
+    }
+
+    @Test
+    public void testBottomButton() {
+        driver = new ChromeDriver();
+        //Ссылка на страницу Яндекс Самокат
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        OrderPageObject orderPageObject = new OrderPageObject(driver);
+
+        //Прокрутка страницы до нижней кнопки "Заказать"
+        orderPageObject.scrollToOrderButtonBottom();
+        //Нажатие кнопки "Заказать" внизу страницы
+        orderPageObject.clickBottomOrderButton();
+
+        test(orderPageObject);
+    }
+
+    private void test(OrderPageObject orderPageObject) {
+        //Ожидание открытия окна офрмления заказа
+        orderPageObject.waitOrderForm();
+        //Установка значения в поле "Имя"
+        orderPageObject.setName(name);
+        //Установка значения в поле "Фамилия"
+        orderPageObject.setSurname(surname);
+        //Установка значения в поле "Адрес"
+        orderPageObject.setAddress(address);
+        //Установка значения в поле "Телефон"
+        orderPageObject.setPhone(phone);
+        //Раскрытие списка станций метро
+        orderPageObject.clickMetroStationDropDown();
+        //Выбор элемента из списка станций метро
+        orderPageObject.clickMetroStationDropDown(metroStation);
+        //Нажатие кнопки "Далее"
+        orderPageObject.clickNextButton();
+        //Ожидание перехода на следующую страницу заполнения заказа
+        orderPageObject.waitAfterClickNextButton();
+        //Нажатие на поле "Дата" с выбором даты доставки самоката
+        orderPageObject.clickDeliveryDatePicker();
+        //Выбор даты в календаре
+        orderPageObject.clickDeliveryDatePicker(day);
+        //Раскрытие списка периодов аренды
+        orderPageObject.clickRentalPeriodDropDown();
+        //Выбор периода аренды из списка периодов аренды
+        orderPageObject.clickRentalPeriodDropDown(rentalPeriod);
+        //Выбор цвета
+        orderPageObject.clickColorCheckBox(color);
+        //Установка значения в поле "Комментарий"
+        orderPageObject.setComment(comment);
+        //Нажатие кнопки "Заказать" в окне заполнения заказа
+        orderPageObject.clickOrderButton();
+        //Ожидание открытия окна подтверждения
+        orderPageObject.waitAfterClickOrderButton();
+        //Нажатие кнопки подтверждения в окне подтверждения
+        orderPageObject.clickConfirmationButton();
+        //Ожидание открытия окна с номером заказа
+        orderPageObject.waitAfterClickConfirmationButton();
+        //Получение header окна с номером заказа
+        WebElement element = orderPageObject.getSuccessfulOrderHeader();
+        //Сравнение ожидаемого текста ответа и полученного
+        assertEquals(EXPECTED, element.getText());
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/example/yandexscootertest/page_object/OrderPageObject.java
@@ -0,0 +1,170 @@
+package com.example.yandexscootertest.page_object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class OrderPageObject {
+
+    private final WebDriver driver;
+
+    //Кнопка "Далее" в форме оформления заказа
+    private final By nextButton = By.className("Button_Middle__1CSJM");
+    //Кнопка "Заказать" в форме оформления заказа
+    private final By orderButton = By.className("Button_Middle__1CSJM");
+    //Кнопка "Заказать" на главной странице сверху
+    private final By topOrderButton = By.className("Button_Button__ra12g");
+    //Drop-down меню (Станция метро) для выбора станции метро (в свёрнутом состоянии)
+    private final By metroStationDropDown = By.className("Order_Text__2broi");
+    //Кнопка "Заказать" на главной странице снизу
+    private final By bottomOrderButton = By.className("Button_Middle__1CSJM");
+    //Кнопка "Да" в окне подтверждения
+    private final By confirmationButton = By.className("Button_Middle__1CSJM");
+    //Drop-down меню (Срок аренды) для выбора срока аренды (в свёрнутом состоянии)
+    private final By rentalPeriodDropDownMenu = By.className("Dropdown-option");
+    //Календарь (Когда привезти самокат) для выбора даты доставки самоката (в свёрнутом состоянии)
+    private final By deliveryDatePicker = By.className("react-datepicker__day");
+    //Элемент drop-down меню (Срок аренды) для выбора срока аренды (в развёрнутом состоянии)
+    private final By rentalPeriodDropDownField = By.className("Dropdown-placeholder");
+    //Header окна, появляющегося, после успешного оформления заказа
+    private final By successfulWindowHeader = By.className("Order_ModalHeader__3FDaJ");
+    //Поле (Имя) для заполнение имени
+    private final By nameTextField = By.xpath(".//input[@placeholder = '* Имя']");
+    //Поле (Фамилия) для заполнение фамилии
+    private final By surnameTextField = By.xpath(".//input[@placeholder = '* Фамилия']");
+    //Поле (Комментарий для курьера) для заполнение коментария
+    private final By commentTextField = By.xpath(".//input[@placeholder = 'Комментарий для курьера']");
+    //Элемент drop-down меню (Станция метро) для выбора станции метро (в развёрнутом состоянии)
+    private final By metroStationDropDownField = By.xpath(".//input[@placeholder = '* Станция метро']");
+    //Check box для выбора цвета
+    private final By colorCheckBox = By.xpath(".//input[@class = 'Checkbox_Input__14A2w']/parent::label");
+    //Поле (Адрес: куда привезти самокат) для заполнение адреса
+    private final By addressTextField = By.xpath(".//input[@placeholder = '* Адрес: куда привезти заказ']");
+    //Дата в календаре (Когда привезти самокат) для выбора даты доставки самоката (в развёрнутом состоянии)
+    private final By deliveryDateCalendarField = By.xpath(".//input[@placeholder = '* Когда привезти самокат']");
+    //Поле (Телефон: на него позвонит курьер) для заполнение номера телефона
+    private final By phoneTextField = By.xpath(".//input[@placeholder = '* Телефон: на него позвонит курьер']");
+
+    public OrderPageObject(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getSuccessfulOrderHeader() {
+        return driver.findElement(successfulWindowHeader);
+    }
+
+    public void scrollToOrderButtonBottom() {
+        WebElement element = driver.findElement(bottomOrderButton);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public void clickNextButton() {
+        driver.findElement(nextButton).click();
+    }
+
+    public void clickTopOrderButton() {
+        driver.findElement(topOrderButton).click();
+    }
+
+    public void clickBottomOrderButton() {
+        driver.findElement(bottomOrderButton).click();
+    }
+
+    public void clickDeliveryDatePicker() {
+        driver.findElement(deliveryDateCalendarField).click();
+    }
+
+    public void clickRentalPeriodDropDown() {
+        driver.findElement(rentalPeriodDropDownField).click();
+    }
+
+    public void clickMetroStationDropDown() {
+        driver.findElement(metroStationDropDownField).click();
+    }
+
+    public void clickColorCheckBox(String color) {
+        clickElementInList(color, colorCheckBox);
+    }
+
+    public void clickConfirmationButton() {
+        clickElementInList("Да", confirmationButton);
+    }
+
+    public void clickMetroStationDropDown(String metroStation) {
+        clickElementInList(metroStation, metroStationDropDown);
+    }
+
+    public void clickDeliveryDatePicker(String day) {
+        clickElementInList(day, deliveryDatePicker);
+    }
+
+    public void clickRentalPeriodDropDown(String rentalPeriod) {
+        clickElementInList(rentalPeriod, rentalPeriodDropDownMenu);
+    }
+
+    public void clickOrderButton() {
+        clickElementInList("Заказать", orderButton);
+    }
+
+    private void clickElementInList(String value, By by) {
+        List<WebElement> elements = driver.findElements(by);
+        for (int i = 0; i < elements.size(); i++) {
+            WebElement element = elements.get(i);
+            if (value.equals(element.getText())) {
+                elements.get(i).click();
+                return;
+            }
+        }
+    }
+
+    public void setName(String name) {
+        setTextValue(name, nameTextField);
+    }
+
+    public void setSurname(String surname) {
+        setTextValue(surname, surnameTextField);
+    }
+
+    public void setAddress(String address) {
+        setTextValue(address, addressTextField);
+    }
+
+    public void setPhone(String phone) {
+        setTextValue(phone, phoneTextField);
+    }
+
+    public void setComment(String comment) {
+        setTextValue(comment, commentTextField);
+    }
+
+    private void setTextValue(String value, By by) {
+        WebElement element = driver.findElement(by);
+        element.sendKeys(value);
+    }
+
+    public void waitOrderForm() {
+        waitElement(nameTextField);
+    }
+
+    public void waitAfterClickOrderButton() {
+        waitElement(confirmationButton);
+    }
+
+    public void waitAfterClickNextButton() {
+        waitElement(deliveryDateCalendarField);
+    }
+
+    public void waitAfterClickConfirmationButton() {
+        waitElement(successfulWindowHeader);
+    }
+
+    private void waitElement(By by) {
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+}
